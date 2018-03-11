5f3216b031b9d01053c55f3648b97db0833c18fc
toraku72 <tora.contact.ch@gmail.com>
2018-03-11 11:33:46 +0700

    Finished the first challenge of chapter 15.

--- app/src/main/java/com/bignerdranch/android/crimminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/crimminalintent/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -111,13 +112,13 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_suspect));
+                ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(getActivity());
+                builder.setType("text/plain");
+                builder.setText(getCrimeReport());
+                builder.setSubject(getString(R.string.crime_report_subject));
+                builder.setChooserTitle(R.string.send_report);
 
-                i= Intent.createChooser(i, getString(R.string.send_report));
-                startActivity(i);
+                builder.startChooser();
             }
         });
 
